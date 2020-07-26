24edc8ffeb33d2505e639d2fa07d143c87ee760e
Wendy Dherin <wendydherin@gmail.com>
2020-07-26 16:04:59 +0100

    Initial commit

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   siteDescription: '',
   siteShortDescription: '',
   siteKeywords: 'comma, separated, list', // Replace this with your SEO keywords
-  siteUrl: 'https://your-username.github.io',
-  pathPrefix: '/my-gatsby-p5-site',
+  siteUrl: 'https://doubledherin.github.io',
+  pathPrefix: '/again',
   siteLanguage: 'en',
   get copyright() {
     return `Copyright \u00A9 ${new Date().getFullYear()} ${this.siteAuthor}`;
