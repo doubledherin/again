f3bc14623cf047e86af5fd9c7cb9045a17a6edba
Wendy Dherin <wendydherin@gmail.com>
2020-07-27 15:18:24 +0100

    Try setting second argument to null to address the __BASE_PATH__ is not defined issue.

--- gatsby-node.js
+++ gatsby-node.js
@@ -50,7 +50,7 @@ exports.createPages = async ({ graphql, actions }) => {
   result.data.allImageSharp.nodes.forEach(node => {
     if (node && node.fields) {
         createPage({
-        path: withPrefix(node.fields.slug),
+        path: withPrefix(node.fields.slug, null),
         component: path.resolve(`./src/templates/galleryItemPage.js`),
         context: {
           // Data passed to context is available
